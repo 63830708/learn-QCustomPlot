9d00fcf76421e362eb1133bb2c6b39560aff617a
xiaosan <63830708@qq.com>
2018-08-27 21:54:40 +0800

    从文件layer开始 添加layermy.h .cpp

---
+++ layermy.cpp
@@ -0,0 +1,57 @@
+﻿#include "layermy.h"
+
+#include "painter.h"
+#include "core.h"
+
+QCPLayer::QCPLayer(QCustomPlot *parentPlot, const QString &layerName):
+    QObject(parentPlot),
+    mParentPlot(parentPlot),
+    mName(layerName),
+    mIndex(-1)
+{
+
+}
+
+QCPLayer::~QCPLayer()
+{
+    while(!mChildren.isEmpty())
+    {
+        mChildren.last()->setLayer(0);
+        if(mParentPlot->currentLayer() == this)
+        {
+            qDebug() << Q_FUNC_INFO << "The parent plots mCurrentLayer will be dangling pointer. Should have been set to a valid layer or 0 beforehand.";
+        }
+    }
+}
+
+
+void QCPLayer::addChild(QCPLayerable *layerable, bool prepend)
+{
+    if( !mChildren.contains(layerable))
+    {
+        if(prepend)
+            mChildren.prepend(layerable);
+        else
+            mChildren.append(layerable);
+    }
+    else
+    {
+        qDebug() << Q_FUNC_INFO  << "layerable is already child of this layer"
+                 << reinterpret_cast<quintptr>(layerable);
+    }
+}
+
+
+void QCPLayer::removeChild(QCPLayerable *layerable)
+{
+    if( !mChildren.removeOne(layerable))
+    {
+        qDebug() << Q_FUNC_INFO << "layerable is not child of this layer"
+                 << reinterpret_cast<quintptr>(layerable);
+    }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////// QCPLayerable
+////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ layermy.h
@@ -0,0 +1,48 @@
+﻿#ifndef LAYERMY_H
+#define LAYERMY_H
+
+
+#include "global.h"
+
+class QCPPainter;
+class QCustomPlot;
+class QCPLayerable;
+class QCPLayoutElement;
+class QCPLayout;
+
+class QCP_LIB_DECL QCPLayer: public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QCustomPlot* parentPlot READ parentPlot)
+    Q_PROPERTY(QString name READ name)
+    Q_PROPERTY(int index READ index)
+    Q_PROPERTY(QList<QCPLayerable*> children READ children)
+public:
+    QCPLayer(QCustomPlot* parentPlot, const QString &layerName);
+    ~QCPLayer();
+
+    //getter
+    QCustomPlot *parentPlot() const { return mParentPlot;}
+    QString name() const {return mName;}
+    int index() const { return mIndex;}
+    QList<QCPLayerable*> children() const { return mChildren;}
+protected:
+    // property
+    QCustomPlot* mParentPlot;
+    QString     mName;
+    int         mIndex;
+    QList<QCPLayerable*>    mChildren;
+
+    void addChild(QCPLayerable *layerable, bool prepend);
+    void removeChild(QCPLayerable* layerable);
+
+private:
+    Q_DISABLE_COPY(QCPLayer)
+
+    friend class QCustomPlot;
+    friend class QCPLayerable;
+};
+
+
+
+#endif // LAYERMY_H
