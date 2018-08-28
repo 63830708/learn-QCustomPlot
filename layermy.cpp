ab9fe96cf80799a822e562e34652ce0488f494e5
chenwenjie <chenwenjie@kongking.net>
2018-08-28 17:17:57 +0800

    继续添加layermy

--- layermy.cpp
+++ layermy.cpp
@@ -3,6 +3,14 @@
 #include "painter.h"
 #include "core.h"
 
+// 5层
+// legend
+// axes
+// main
+// grid   QCPGrid实例属于QCPAxis     QCPAxis::grid
+// background   QCPAxisRect实例
+
+// 不应该直接构造，应该由QCustomPlot::addLayer
 QCPLayer::QCPLayer(QCustomPlot *parentPlot, const QString &layerName):
     QObject(parentPlot),
     mParentPlot(parentPlot),
@@ -12,6 +20,7 @@ QCPLayer::QCPLayer(QCustomPlot *parentPlot, const QString &layerName):
 
 }
 
+// 应该由QCustomPlot::removeLayer 删除 这样层中的每个layerable先从层中移除，再删除层
 QCPLayer::~QCPLayer()
 {
     while(!mChildren.isEmpty())
@@ -55,3 +64,155 @@ void QCPLayer::removeChild(QCPLayerable *layerable)
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////// QCPLayerable
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+QCPLayerable::QCPLayerable(QCustomPlot *plot, QString targetLayer, QCPLayerable *parentLayerable):
+    QObject(plot),
+    mVisible(true),
+    mParentPlot(plot),
+    mParentLayeralbe(parentLayerable),
+    mLayer(0),
+    mAntialiased(true)
+{
+    if(mParentPlot)
+    {
+        if(targetLayer.isEmpty())
+            setLayer(mParentPlot->currentLayer());
+        else if(!setLayer(targetLayer))
+            qDebug() << Q_FUNC_INFO << "setting QCPLayerable initial layer to " << targetLayer << "Failed.";
+    }
+}
+
+QCPLayerable::~QCPLayerable()
+{
+    if(mLayer)
+    {
+        mLayer->removeChild(this);
+        mLayer = 0;
+    }
+}
+
+void QCPLayerable::setVisible(bool on)
+{
+    mVisible = on;
+}
+
+bool QCPLayerable::setLayer(QCPLayer *layer)
+{
+    return moveToLayer(layer, false);
+}
+
+
+bool QCPLayerable::setLayer(const QString &layerName)
+{
+    if( !mParentPlot)
+    {
+        qDebug() << Q_FUNC_INFO << "no parent QCustomPlot set.";
+        return false;
+    }
+    if(QCPLayer* layer = mParentPlot->layer(layerName))
+    {
+        return setLayer(layer);
+    }
+    else
+    {
+        qDebug() << Q_FUNC_INFO << "there's no layer with name " << layerName;
+        return false;
+    }
+}
+
+void QCPLayerable::setAntialiased(bool enabled)
+{
+    mAntialiased = enabled;
+}
+
+bool QCPLayerable::realVisibility() const
+{
+    return mVisible && (!mParentLayerable || mParentLayerable.data()->realVisibility());
+}
+
+
+double QCPLayerable::selectTest(const QPointF &pos, bool onlySelectable, QVariant *details) const
+{
+    Q_UNUSED(pos);
+    Q_UNUSED(onlySelectable);
+    Q_UNUSED(details);
+    return -1.0;
+}
+
+
+void QCPLayerable::initializeParentPlot(QCustomPlot *parentPlot)
+{
+    if(mParentPlot)
+    {
+        qDebug() << Q_FUNC_INFO << "called with mParentPlot already initialized.";
+        return;
+    }
+
+    if(!parentPlot)
+        qDebug() << Q_FUNC_INFO << "called with parentPlot zero.";
+
+    mParentPlot = parentPlot;
+    parentPlotInitialized(mParentPlot);
+}
+
+
+void QCPLayerable::setParentLayerable(QCPLayerable *parentLayerable)
+{
+    mParentLayerable = parentLayerable;
+}
+
+
+bool QCPLayerable::moveToLayer(QCPLayer *layer, bool prepend)
+{
+    if( layer && !mParentPlot)
+    {
+        qDebug() << Q_FUNC_INFO << "no parent QCustomPlot set";
+        return false;
+    }
+    if( layer && layer->parentPlot() !=mParentPlot)
+    {
+        qDebug() << Q_FUNC_INFO << "layer " << layer->name() << "is not in same QCustomPlot as this layerable.";
+        return false;
+    }
+
+    if( mLayer)
+        mLayer->removeChild(this);
+
+    mLayer = layer;
+    if(mLayer)
+        mLayer->addChild(this, prepend);
+
+    return true;
+}
+
+
+void QCPLayerable::applyAntialiasingHint(QCPPainter *painter, bool localAntialiased, QCP::AntialiasedElement overrideElement) const
+{
+    if(mParentPlot && mParentPlot->notAntialiasedElements().testFlag(overrideElement))
+        painter->setAntialiasing(false);
+    else if( mParentPlot && mParentPlot->antialiasedElements().testFlag(overrideElement))
+        painter->setAntialiasing(true);
+    else
+        painter->setAntialiasing(localAntialiased);
+}
+
+
+// 被initializeParentPlot调用
+void QCPLayerable::parentPlotInitialized(QCustomPlot *parentPlot)
+{
+    Q_UNUSED(parentPlot);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- layermy.h
+++ layermy.h
@@ -44,5 +44,64 @@ private:
 };
 
 
+class QCP_LIB_DECL QCPLayerable: public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(bool visible READ visible WRITE setVisible)
+    Q_PROPERTY(QCustomPlot* parentPlot READ parentPlot)
+    Q_PROPERTY(QCPLayerable* parentLayerable READ parentLayerable)
+    Q_PROPERTY(QCPLayer* layer READ layer WRITE setLayer)
+    Q_PROPERTY(bool antialiased READ antialiased WRITE setAntialiased)
+
+public:
+    QCPLayerable(QCustomPlot *plot, QString targetLayer="", QCPLayerable *parentLayerable);
+    ~QCPLayerable();
+
+    bool visible() const { return mVisible;}
+    QCustomPlot *parentPlot() const { return mParentPlot;}
+    QCPLayerable *parentLayerable() const { return mParentLayerable.data();}
+    QCPLayer *layer() const { return mLayer;}
+    bool antialiased() const { return mAntialiased;}
+
+    void setVisible(bool on);
+    bool setLayer(QCPLayer *layer);
+    bool setLayer(const QString &layerName);
+    void setAntialiased(bool enabled);
+
+    virtual double selectTest(const QPointF &pos, bool onlySelectable, QVariant *details=0) const;
+
+    bool realVisibility() const;
+
+protected:
+    // property
+    bool mVisible;
+    QCustomPlot *mParentPlot;
+    QWeakPointer<QCPLayerable> mParentLayerable;
+    QCPLayer *mLayer;
+    bool mAntialiased;
+
+    virtual void parentPlotInitialized(QCustomPlot *parentPlot);
+    virtual QCP::Interaction selectionCategory() const;
+    virtual QRect clipRect() const;
+    virtual void applyDefaultAntialiasingHint(QCPPainter *painter) const;
+    virtual void draw(QCPPainter *painter) = 0;
+
+    // events
+    virtual void selectEvent(QMouseEvent *event, bool additive, const QVariant &details, bool *selectionStateChanged);
+    virtual void deselectEvent(bool *selectionStateChanged);
+
+    //
+    void initializeParentPlot(QCustomPlot *parentPlot);
+    void setParentLayerable(QCPLayerable *parentLayerable);
+    bool moveToLayer(QCPLayer *layer, bool prepend);
+    void applyAntialiasingHint(QCPPainter* painter, bool localAntialiased, QCP::AntialiasedElement overrideElement) const;
+
+private:
+    Q_DISABLE_COPY(QCPLayerable)
+
+    friend class QCustomPlot;
+    friend class QCPAxisRect;
+};
+
 
 #endif // LAYERMY_H
